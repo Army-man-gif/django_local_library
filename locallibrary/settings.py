e0208f95feb31071fcc68b38cf02936aae374054
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-04 00:05:19 +0100

    modified settings to configure for heroku deployment

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2i4$w@ww9(qqa81=+$rvy)4_*m+!d4kvlmqn!$+iwt3baa8omn"
-
+SECRET_KEY = os.environ.get("SECRET_KEY", "your-default-dev-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testerapp.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
